a405c50a60b1dc40cf029f33eedb05d2523daf6a
EvgeniySkvortsov <e.skvortsov@chillgaming.net>
2024-04-11 17:30:29 +0300

    add GetCardLog and GetCardsLog

--- server/Code/GlobalUtils/Logger.cs
+++ server/Code/GlobalUtils/Logger.cs
@@ -1,3 +1,4 @@
+using server.Code.MorpehFeatures.RoomPokerFeature.Enums;
 using server.Code.MorpehFeatures.RoomPokerFeature.Models;
 
 namespace server.Code.GlobalUtils;
@@ -40,8 +41,90 @@ public static class Logger
         }
     }
 
-    public static string GetCardsLog(List<CardModel> cards, string sf = "")
+    public static string GetCardsLog(List<CardModel> cardModels, string handSymbol = "")
     {
-        return "";
+        var result = string.Empty;
+
+        foreach (var card in cardModels)
+        {
+            result += GetCardLog(card, handSymbol) + " ";
+        }
+
+        return result;
+    }
+
+    public static string GetCardLog(CardModel cardModel, string handSymbol = "")
+    {
+        var result = string.Empty;
+
+        var rank = cardModel.Rank;
+        var suit = cardModel.Suit;
+        var isHand = cardModel.IsHands;
+
+        switch (rank)
+        {
+            case CardRank.Two:
+                result += "2";
+                break;
+            case CardRank.Three:
+                result += "3";
+                break;
+            case CardRank.Four:
+                result += "4";
+                break;
+            case CardRank.Five:
+                result += "5";
+                break;
+            case CardRank.Six:
+                result += "6";
+                break;
+            case CardRank.Seven:
+                result += "7";
+                break;
+            case CardRank.Eight:
+                result += "8";
+                break;
+            case CardRank.Nine:
+                result += "9";
+                break;
+            case CardRank.Ten:
+                result += "10";
+                break;
+            case CardRank.Jack:
+                result += "J";
+                break;
+            case CardRank.Queen:
+                result += "Q";
+                break;
+            case CardRank.King:
+                result += "K";
+                break;
+            case CardRank.Ace:
+                result += "A";
+                break;
+        }
+        
+        switch (suit)
+        {
+            case CardSuit.Hearts:
+                result += "♥";
+                break;
+            case CardSuit.Spades:
+                result += "♠";
+                break;
+            case CardSuit.Diamonds:
+                result += "♦";
+                break;
+            case CardSuit.Clubs:
+                result += "♣";
+                break;
+        }
+
+        if (isHand)
+        {
+            result += handSymbol;
+        }
+
+        return result;
     }
 }
