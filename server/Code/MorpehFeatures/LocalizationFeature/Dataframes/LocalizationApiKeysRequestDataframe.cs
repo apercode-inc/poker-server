f4e64fd1897c680bdef49b0b8f8477df7ac7577e
Alexander Litskevich <a.litskevich@chillgaming.net>
2024-06-17 22:33:22 +0400

    Added localization api send response

---
+++ server/Code/MorpehFeatures/LocalizationFeature/Dataframes/LocalizationApiKeysRequestDataframe.cs
@@ -0,0 +1,15 @@
+﻿using NetFrame;
+using NetFrame.WriteAndRead;
+
+namespace server.Code.MorpehFeatures.LocalizationFeature.Dataframes;
+
+public struct LocalizationApiKeysRequestDataframe : INetworkDataframe
+{
+    public void Write(NetFrameWriter writer)
+    {
+    }
+
+    public void Read(NetFrameReader reader)
+    {
+    }
+}
