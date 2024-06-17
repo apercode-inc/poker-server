9d55649ccda7f453f0e5b31a296f83cf9e59bff8
Alexander Litskevich <a.litskevich@chillgaming.net>
2024-06-17 20:44:49 +0400

    Updated localization in notifications

---
+++ server/Code/GlobalUtils/LocalizationKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Code.GlobalUtils;
+
+public static class LocalizationKeys
+{
+    public const string RoomPokerJoinNotEnoughMoney = "$ROOM_POKER_JOIN_NOT_ENOUGH_MONEY";
+    public const string RoomPokerJoinNoFreeSpace = "$ROOM_POKER_JOIN_NO_FREE_SPACE";
+    public const string AuthPlayerCreateAlreadyExists = "$AUTH_PLAYER_CREATE_ALREADY_EXISTS";
+    public const string AuthPlayerCreateError = "$AUTH_PLAYER_CREATE_ERROR";
+    public const string AuthPlayerCreateIncorrectAvatar = "$AUTH_PLAYER_CREATE_INCORRECT_AVATAR";
+    public const string AuthPlayerCreateNicknameTooShort = "$AUTH_PLAYER_CREATE_NICKNAME_LENGTH_TOO_SHORT";
+}
